098965f3b86d1ebe1303c900f29a855818cd0d95
KennethMoons <kenneth.moons1@gmail.com>
2016-12-08 19:02:17 +0100

    update App

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,11 +1,22 @@
 var myApp = angular.module('myApp', ['ui.router']);
 
+myApp.directive('errorMessage',function(){
+  return {
+        template : "{{errorMsg}}"
+    };
+});
+
+
 myApp.config(function($stateProvider, $urlRouterProvider) {
     
-    $urlRouterProvider.otherwise('/home');
+    $urlRouterProvider.otherwise('/aanmelden');
     
     $stateProvider
-        
+
+        .state('aanmelden', {
+          url: '/aanmelden',
+          templateUrl: 'aanmelden.html'
+        })
         .state('home', {
             url: '/home',
             templateUrl: 'home.html'
@@ -21,6 +32,10 @@ myApp.config(function($stateProvider, $urlRouterProvider) {
         .state('update', {
             url: '/update',
             templateUrl: 'update.html'      
+        })
+        .state('registreer', {
+            url: '/registreer',
+            templateUrl: 'registreer.html'      
         });
         
 });
@@ -28,15 +43,31 @@ myApp.config(function($stateProvider, $urlRouterProvider) {
 myApp.controller('AppCtrl', ['$scope', '$http','$location', function($scope, $http,$location) {
     console.log("Hello World from controller");
     $scope.bluray = {};
+    $scope.bluraylist = {};
+    $scope.gebruikerRegistratie = {};
+    $scope.preAanmelding = {};
+    var postAanmelding = {};
+    $scope.errorMsg = "";
+
+     $scope.video = 
+    {
+        "url": ""
+    }
+    ;
 
 var refresh = function() {
-  $http.get('/bluraylist').success(function(response) {
+  $http.get('/blurays').success(function(response) {
     console.log("I got the data I requested");
-    $scope.bluraylist = response;
+    var gebruikerblurayList = response;
+    var teller = 0;
+    for(var i = 0 ; i < gebruikerblurayList.length;i++){
+      if(gebruikerblurayList[i].gebruiker == $scope.bluray.gebruiker){
+        $scope.bluraylist[teller] = gebruikerblurayList[i];
+        teller++;
+      };
+    };
   });
 };
-
-refresh();
 
 $scope.goToCreate = function(){
 	$location.path('/create');
@@ -44,25 +75,28 @@ $scope.goToCreate = function(){
 
 $scope.addBluray = function() {
   console.log($scope.bluray);
-  $http.post('/bluraylist', $scope.bluray).success(function(response) {
+  $http.post('/blurays', $scope.bluray).success(function(response) {
     console.log(response);
+    var gebruikerTemp = $scope.bluray.gebruiker;
     $location.path('/home');
     $scope.bluray = {};
+    $scope.bluray.gebruiker = gebruikerTemp;
     refresh();
   });
 };
 
 $scope.remove = function(id) {
   console.log(id);
-  $http.delete('/bluraylist/' + id).success(function(response) {
+  $http.delete('/blurays/' + id).success(function(response) {
     refresh();
   });
 };
 
 $scope.edit = function(id) {
   console.log(id);
-  $http.get('/bluraylist/' + id).success(function(response) {
+  $http.get('/blurays/' + id).success(function(response) {
     $scope.bluray1 = response;
+    $scope.video.url = $scope.bluray1.youtubelink;
     $location.path('/details');
 
   });
@@ -70,7 +104,7 @@ $scope.edit = function(id) {
 
 $scope.updatePagina = function(id) {
   console.log(id);
-  $http.get('/bluraylist/' + id).success(function(response) {
+  $http.get('/blurays/' + id).success(function(response) {
     $scope.bluray1 = response;
     $location.path('/update');
 
@@ -82,11 +116,50 @@ $scope.cancel = function(){
 
 $scope.update = function() {
   console.log($scope.bluray1._id);
-  $http.put('/bluraylist/' + $scope.bluray1._id, $scope.bluray1).success(function(response) {
+  $http.put('/blurays/' + $scope.bluray1._id, $scope.bluray1).success(function(response) {
     $location.path('/home');
     $scope.bluray1 = {};
     refresh();
   })
 };
 
+$scope.Registreren =  function(){
+  $location.path('/registreer');
+};
+$scope.AnnulerenRegistratie = function(){
+  $location.path('/aanmelden');
+};
+$scope.addGebruiker = function() {
+  console.log($scope.gebruikerRegistratie);
+  $http.post('/gebruikers', $scope.gebruikerRegistratie).success(function(response) {
+    $location.path('/aanmelden');
+    $scope.gebruikerRegistratie = {};
+  });
+};
+
+$scope.LogOn = function(){
+  $http.get('/gebruikers').success(function(response) {
+    console.log("I got the data I requested");
+    postAanmelding = response;
+    for(var i = 0 ; i < postAanmelding.length;i++){
+      if(postAanmelding[i].email == $scope.preAanmelding.email && postAanmelding[i].wachtwoord == $scope.preAanmelding.wachtwoord){
+        $scope.bluray.gebruiker = postAanmelding[i]._id;
+        refresh();
+        $location.path('/home');
+        return;
+      };
+    };
+  });
+  $scope.errorMsg = "aanmelden niet gelukt. wachtwoord of email niet in orde";
+  $location.path('/aanmelden');
+};
+
 }]);﻿
+
+myApp.filter('trusted', ['$sce', function ($sce) {
+        return function(url) {
+        		var video_id = url.split('v=')[1].split('&')[0];
+            return $sce.trustAsResourceUrl("https://www.youtube.com/embed/" + video_id);
+        };
+    }]);
+
--- server.js
+++ server.js
@@ -1,52 +1,70 @@
-// MEAN Stack RESTful API Tutorial - Contact List App
-
 var express = require('express');
 var app = express();
 var mongojs = require('mongojs');
-var db = mongojs('bluraylist', ['bluraylist']);
+var collections = ["gebruikers","blurays"]
+var db = mongojs('mongodb://kenneth:telenet2017@ds119368.mlab.com:19368/bluraylist', collections);
 var bodyParser = require('body-parser');
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 
-app.get('/bluraylist', function (req, res) {
+
+
+app.get('/gebruikers', function (req, res) {
   console.log('I received a GET request');
 
-  db.bluraylist.find(function (err, docs) {
+  db.gebruikers.find(function (err, docs) {
     console.log(docs);
     res.json(docs);
   });
 });
 
-app.post('/bluraylist', function (req, res) {
+app.get('/blurays', function (req, res) {
+  console.log('I received a GET request');
+
+  db.blurays.find(function (err, docs) {
+    console.log(docs);
+    res.json(docs);
+  });
+});
+
+
+app.post('/blurays', function (req, res) {
   console.log(req.body);
-  db.bluraylist.insert(req.body, function(err, doc) {
+  db.blurays.insert(req.body, function(err, doc) {
+    res.json(doc);
+  });
+});
+
+app.post('/gebruikers', function (req, res) {
+  console.log(req.body);
+  db.gebruikers.insert(req.body, function(err, doc) {
     res.json(doc);
   });
 });
 
 
 
-app.delete('/bluraylist/:id', function (req, res) {
+app.delete('/blurays/:id', function (req, res) {
   var id = req.params.id;
   console.log(id);
-  db.bluraylist.remove({_id: mongojs.ObjectId(id)}, function (err, doc) {
+  db.blurays.remove({_id: mongojs.ObjectId(id)}, function (err, doc) {
     res.json(doc);
   });
 });
 
-app.get('/bluraylist/:id', function (req, res) {
+app.get('/blurays/:id', function (req, res) {
   var id = req.params.id;
   console.log(id);
-  db.bluraylist.findOne({_id: mongojs.ObjectId(id)}, function (err, doc) {
+  db.blurays.findOne({_id: mongojs.ObjectId(id)}, function (err, doc) {
     res.json(doc);
   });
 });
 
-app.put('/bluraylist/:id', function (req, res) {
+app.put('/blurays/:id', function (req, res) {
   var id = req.params.id;
   console.log(req.body.name);
-  db.bluraylist.findAndModify({
+  db.blurays.findAndModify({
     query: {_id: mongojs.ObjectId(id)},
     update: {$set: {titel: req.body.titel, genre: req.body.genre, beoordeling: req.body.beoordeling, kostprijs: req.body.kostprijs, voornaamste_acteur: req.body.voornaamste_acteur, voornaamste_actrice: req.body.voornaamste_actrice, youtubelink: req.body.youtubelink}},
     new: true}, function (err, doc) {
